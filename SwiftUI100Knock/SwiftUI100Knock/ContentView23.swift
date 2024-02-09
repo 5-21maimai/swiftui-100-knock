c5cb880610fe32e0a1758b96e3910e809a92187a
5-21maimai <my.mai521@gmail.com>
2024-02-09 23:07:13 +0900

    問題23修正

--- SwiftUI100Knock/SwiftUI100Knock/ContentView23.swift
+++ SwiftUI100Knock/SwiftUI100Knock/ContentView23.swift
@@ -10,24 +10,49 @@ import SwiftUI
 struct ContentView23: View {
 
     var body: some View {
-        VStack(alignment: .leading) {
-            CardView {
+        VStack(alignment: .center, spacing: 16) {
+            CardView(shadowColor: .red) {
                 Text("hoge")
                 Text("fuga")
             }
-        }.padding()
+            
+            CardView(shadowColor: .green) {
+                Text("hoge")
+                Text("fuga")
+            }
+            
+            CardView(shadowColor: .blue) {
+                Image("Panda")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+            }
+        }
     }
 }
 
 struct CardView<Content: View>: View {
+    var shadowColor: Color
     var content: Content
         
-    init(@ViewBuilder  content: () -> Content) {
+    init(
+        shadowColor: Color,
+        @ViewBuilder  content: () -> Content
+    ) {
+        self.shadowColor = shadowColor
         self.content = content()
     }
     
     var body: some View {
-        content.background(.red)
+        content
+            .padding(16)
+            .background(Color.white)
+            .cornerRadius(16)
+            .shadow(
+                color: shadowColor,
+                radius: 4,
+                x: 4,
+                y: 4
+            )
     }
 }
 
