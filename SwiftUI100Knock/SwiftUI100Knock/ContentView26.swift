dbf73a777f4a12a91ebebdeb27564465d50f9402
5-21maimai <my.mai521@gmail.com>
2024-02-11 22:54:54 +0900

    問題26途中

---
+++ SwiftUI100Knock/SwiftUI100Knock/ContentView26.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView26.swift
+//  SwiftUI100Knock
+//
+//  Created by 浅井麻衣 on 2024/02/11.
+//
+
+import SwiftUI
+
+
+struct ContentView26: View {
+    @State private var contents = ["Apple", "Orange", "Grape"]
+    @State private var showAlert = false
+
+    var body: some View {
+        List {
+            ForEach(contents, id: \.self) { content in
+                Button {
+                    self.showAlert = true
+                } label: {
+                    Text(content)
+                        .foregroundColor(.black)
+                }
+            }.onDelete(perform: { indexSet in
+                contents.remove(atOffsets: indexSet)
+            })
+        }.alert(
+            "title",
+            isPresented: $showAlert
+        ) {
+            Button("Button1", role: .destructive) {
+                
+            }
+        } message: {
+            Text("message")
+        }
+    }
+}
+
+#Preview {
+    ContentView26()
+}
