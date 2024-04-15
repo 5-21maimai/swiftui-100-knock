5db9bc3f1dd4064ff93bbb95a59c8de4ad9a790d
5-21maimai <my.mai521@gmail.com>
2024-04-15 21:55:28 +0900

    問題29

--- SwiftUI100Knock/SwiftUI100Knock/ContentView29.swift
+++ SwiftUI100Knock/SwiftUI100Knock/ContentView29.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView29: View {
     @State private var selectionColor: Color = .red
+    @State private var selectionNumber: Int = 0
 
     var body: some View {
         HStack {
@@ -18,10 +19,10 @@ struct ContentView29: View {
                 Text("緑").tag(Color.green)
             }.pickerStyle(.wheel)
             
-            Picker("色を選択", selection: $selectionColor) {
-                Text("赤").tag(Color.red)
-                Text("青").tag(Color.blue)
-                Text("緑").tag(Color.green)
+            Picker("色を選択", selection: $selectionNumber) {
+                Text("0").tag(0)
+                Text("1").tag(1)
+                Text("2").tag(2)
             }.pickerStyle(.wheel)
         }
     }
